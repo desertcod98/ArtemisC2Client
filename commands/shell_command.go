5d7d34b87a18a0f58bf4275ceb0da4636093f545
desertcod98 <desertcod98@gmail.com>
2026-02-01 19:11:46 +0100

    [Shell] Use cmd.exe output

--- commands/shell_command.go
+++ commands/shell_command.go
@@ -4,16 +4,19 @@ import (
 	"bytes"
 	"io"
 	"os/exec"
+	"strings"
 )
 
 type ShellCommand struct{}
 
 func (c ShellCommand) Execute(args []string) (io.ReaderAt, uint64) {
-    var out []byte
-    var err error
-    out, err = exec.Command(args[0], args[1:]...).Output()
-    if err != nil {
-        out = []byte(err.Error())
-    }
-    return bytes.NewReader(out), uint64(len(out))
+	var out []byte
+	var err error
+
+	cmdStr := strings.Join(args, " ")
+	out, err = exec.Command("cmd.exe", "/C", cmdStr).Output()
+	if err != nil {
+		out = []byte(err.Error())
+	}
+	return bytes.NewReader(out), uint64(len(out))
 }
